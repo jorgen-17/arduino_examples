4a63a89a5ae7dc27c7d7c59683896b23270a1cd9
Jorge Nonell <jorge_nonell@ultimatesoftware.com>
2024-06-21 18:15:52 -0400

    Adding ascending trill variation

--- tone_experiments/tone_experiments.ino
+++ tone_experiments/tone_experiments.ino
@@ -13,9 +13,9 @@ uint8_t buzzButton = 4;
 uint8_t buzzer = 11;
 
 void buzzUp (
-  int (*changeFunc) (int note, uint8_t changeAmount, int initialNote),
-  uint8_t changeAmount, int initialNote, int finalNote) {
-  int note = initialNote;
+  uint16_t (*changeFunc) (uint16_t note, uint8_t changeAmount, uint16_t initialNote),
+  uint8_t changeAmount, uint16_t initialNote, uint16_t finalNote) {
+  uint16_t note = initialNote;
   while (note < finalNote) {
     tone(buzzer, note, noteDuration * 0.9);
     delay(noteDuration);
@@ -25,9 +25,9 @@ void buzzUp (
 }
 
 void buzzDown (
-  int (*changeFunc) (int note, uint8_t changeAmount, int initialNote),
-  uint8_t changeAmount, int initialNote, int finalNote) {
-  int note = initialNote;
+  uint16_t (*changeFunc) (uint16_t note, uint8_t changeAmount, uint16_t initialNote),
+  uint8_t changeAmount, uint16_t initialNote, uint16_t finalNote) {
+  uint16_t note = initialNote;
   while (note > finalNote) {
     tone(buzzer, note, noteDuration * 0.9);
     delay(noteDuration);
@@ -36,15 +36,15 @@ void buzzDown (
   }
 }
 
-int halfStepUp (int note, uint8_t changeAmount, int initialNote) {
+uint16_t halfStepChange (uint16_t note, uint8_t changeAmount, uint16_t initialNote) {
   return halfStep;
 }
 
-int wholeStepUp (int note, uint8_t changeAmount, int initialNote) {
+uint16_t wholeStepChange (uint16_t note, uint8_t changeAmount, uint16_t initialNote) {
   return wholeStep;
 }
 
-int upWholeDownHalf (int note, uint8_t changeAmount, int initialNote) {
+uint16_t upWholeDownHalf (uint16_t note, uint8_t changeAmount, uint16_t initialNote) {
   if (stepUp) {
     stepUp = false;
     return wholeStep * 3;
@@ -54,8 +54,8 @@ int upWholeDownHalf (int note, uint8_t changeAmount, int initialNote) {
 }
 
 // fibonacci series
-int buzzonacci (int note, uint8_t changeAmount, int initialNote) {
-  int current;
+uint16_t buzzonacci (uint16_t note, uint8_t changeAmount, uint16_t initialNote) {
+  uint16_t current;
   if (note == initialNote) {
     previous = 1;
     current = 1;
@@ -67,7 +67,7 @@ int buzzonacci (int note, uint8_t changeAmount, int initialNote) {
 }
 
 // catalan numbers series
-int buzzalan (int note, uint8_t changeAmount, int initialNote) {
+uint16_t buzzalan (uint16_t note, uint8_t changeAmount, uint16_t initialNote) {
   if (note == initialNote) {
     previous = 1;
   }
@@ -75,7 +75,7 @@ int buzzalan (int note, uint8_t changeAmount, int initialNote) {
   return previous * changeAmount;
 }
 
-int squareBuzz (int note, uint8_t changeAmount, int initialNote) {
+uint16_t squareBuzz (uint16_t note, uint8_t changeAmount, uint16_t initialNote) {
   if (note == initialNote) {
     previous = 1;
   }
@@ -83,12 +83,47 @@ int squareBuzz (int note, uint8_t changeAmount, int initialNote) {
   return previous * changeAmount;
 }
 
-int triangleBuzz (int note, uint8_t changeAmount, int initialNote) {
+uint16_t triangleBuzz (uint16_t note, uint8_t changeAmount, uint16_t initialNote) {
   if (note == initialNote) {
     previous = 1;
   }
   previous += (previous * (previous + 1)) / 2;
   return previous * changeAmount;
+}
+
+void franticRobot () {
+  uint16_t start = 223;
+  uint16_t end = 762;
+  uint8_t sequence[11] = { 1, 2, 3, 5, 8, 13, 8, 5, 3, 2, 1 };
+  uint8_t sequenceLength = sizeof(sequence) / sizeof(sequence[0]);
+  for (uint8_t i = 0; i < sequenceLength; i++) {
+    uint16_t pitchChange = halfStep * sequence[i];
+    if (pitchChange % 2) {
+      start += pitchChange;
+      end += pitchChange;
+      buzzDown(buzzonacci, seventhStep, end, start);
+    } else {
+      start -= pitchChange;
+      end -= pitchChange;
+      buzzUp(buzzalan, seventhStep, start, end);
+    }
+  }
+}
+
+void ascendingTrill () {
+  uint16_t start = 223;
+  uint16_t end = 762;
+  uint8_t prev = 1;
+  uint8_t curr = 1;
+  while (curr <= 21) {
+    uint16_t pitchChange = halfStep * curr;
+    start += pitchChange;
+    end += pitchChange;
+    buzzUp(wholeStepChange, seventhStep, start, end);
+    delay(delayTime);
+    prev = curr;
+    curr += prev;
+  }
 }
 
 void setup () {
@@ -101,23 +136,7 @@ void setup () {
 void loop () {
   byte buzzButtonState = digitalRead(buzzButton);
   if (!buzzButtonState) {
-    Serial.println("buzz buzz");
-    int start = 223;
-    int end = 762;
-    uint8_t sequence[11] = { 1, 2, 3, 5, 8, 13, 8, 5, 3, 2, 1 };
-    uint8_t sequenceLength = sizeof(sequence) / sizeof(sequence[0]);
-    for (uint8_t i = 0; i < sequenceLength; i++) {
-      int pitchChange = halfStep * sequence[i];
-      if (pitchChange % 2) {
-        start += pitchChange;
-        end += pitchChange;
-        buzzDown(buzzonacci, seventhStep, end, start);
-      } else {
-        start -= pitchChange;
-        end -= pitchChange;
-        buzzUp(buzzalan, seventhStep, start, end);
-      }
-    }
+    ascendingTrill();
   }
 
   delay(delayTime);
